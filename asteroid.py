6352df546205ac08a7f2d837aba7ee98c31bcf29
Daniel Krusky <dkrusky@microvb.com>
2025-01-10 04:39:19 +0000

    saving

---
+++ asteroid.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import *
+
+class CircleShape:
+    def __init__(self, x, y, radius):
+        self.position = pygame.Vector2(x, y)
+        self.radius = radius
+        self.velocity = pygame.Vector2(0, 0)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 255, 255), self.position, self.radius, 2)
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 255, 255), self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -3,19 +3,45 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable.add(player)
+    drawable.add(player)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))  # Fill the screen with black
+        
+        for obj in updatable:
+            obj.update(dt)  # Update all updatable objects
+        for obj in drawable:
+            obj.draw(screen)  # Draw all drawable objects
+
         pygame.display.flip()  # Refresh the screen
+        dt = clock.tick(60) / 1000  # Restrict to 60 FPS and get delta time
 
 if __name__ == "__main__":
     main()
